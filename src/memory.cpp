82b32271085159012ce9dca23a978a84d4374d61
Matt Muroya <13994435+mattmuroya@users.noreply.github.com>
2025-10-18 18:57:00 -1000

    Remove unnecessary `this` pointers

--- src/memory.cpp
+++ src/memory.cpp
@@ -4,10 +4,10 @@
 
 uint8_t Memory::read(uint16_t address)
 {
-    return this->data[address];
+    return data[address];
 }
 
 void Memory::write(uint16_t address, uint8_t value)
 {
-    this->data[address] = value;
+    data[address] = value;
 }
